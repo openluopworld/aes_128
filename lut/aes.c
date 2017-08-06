74fb9ca30b64d91db22458b1c2f8185770799c0f
openluopworld <luopengxq@gmail.com>
2017-08-06 11:43:07 +0800

    add encryption of lut

--- lut/aes.c
+++ lut/aes.c
@@ -9,7 +9,12 @@
 #include <stdint.h>
 #include <stdio.h>
 
-static const uint32_t T1[256] = {
+#include "aes.h"
+
+/*
+ * Four encryption tables
+ */
+static const uint32_t T0[256] = {
 	0xa56363c6, 0x847c7cf8, 0x997777ee, 0x8d7b7bf6, 
 	0xdf2f2ff,  0xbd6b6bd6, 0xb16f6fde, 0x54c5c591, 
 	0x50303060, 0x3010102,  0xa96767ce, 0x7d2b2b56, 
@@ -76,7 +81,7 @@ static const uint32_t T1[256] = {
 	0xcbb0b07b, 0xfc5454a8, 0xd6bbbb6d, 0x3a16162c, };
 
 
-static const uint32_t T2[256] = {
+static const uint32_t T1[256] = {
 	0x6363c6a5, 0x7c7cf884, 0x7777ee99, 0x7b7bf68d, 
 	0xf2f2ff0d, 0x6b6bd6bd, 0x6f6fdeb1, 0xc5c59154, 
 	0x30306050, 0x1010203,  0x6767cea9, 0x2b2b567d, 
@@ -143,7 +148,7 @@ static const uint32_t T2[256] = {
 	0xb0b07bcb, 0x5454a8fc, 0xbbbb6dd6, 0x16162c3a, };
 
 
-static const uint32_t T3[256] = {
+static const uint32_t T2[256] = {
 	0x63c6a563, 0x7cf8847c, 0x77ee9977, 0x7bf68d7b, 
 	0xf2ff0df2, 0x6bd6bd6b, 0x6fdeb16f, 0xc59154c5, 
 	0x30605030, 0x1020301,  0x67cea967, 0x2b567d2b, 
@@ -210,7 +215,7 @@ static const uint32_t T3[256] = {
 	0xb07bcbb0, 0x54a8fc54, 0xbb6dd6bb, 0x162c3a16, };
 
 
-static const uint32_t T4[256] = {
+static const uint32_t T3[256] = {
 	0xc6a56363, 0xf8847c7c, 0xee997777, 0xf68d7b7b, 
 	0xff0df2f2, 0xd6bd6b6b, 0xdeb16f6f, 0x9154c5c5, 
 	0x60503030, 0x2030101,  0xcea96767, 0x567d2b2b, 
@@ -276,3 +281,121 @@ static const uint32_t T4[256] = {
 	0x82c34141, 0x29b09999, 0x5a772d2d, 0x1e110f0f, 
 	0x7bcbb0b0, 0xa8fc5454, 0x6dd6bbbb, 0x2c3a1616, };
 
+
+/*
+ * round constants
+ */
+static uint8_t RC[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36};
+
+/*
+ * Sbox
+ */
+static uint8_t SBOX[256] = {
+    0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
+    0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+    0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+    0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+    0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+    0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+    0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+    0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+    0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+    0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+    0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+    0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+    0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+    0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+    0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+    0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16};
+
+void aes_key_schedule_128(const uint8_t *key, uint8_t *roundkeys) {
+
+    uint8_t temp[4];
+    uint8_t *last4bytes; // point to the last 4 bytes of one round
+    uint8_t *lastround;
+    uint8_t i;
+
+    for (i = 0; i < 16; ++i) {
+        *roundkeys++ = *key++;
+    }
+
+    last4bytes = roundkeys-4;
+    for (i = 0; i < AES_ROUNDS; ++i) {
+        // k0-k3 for next round
+        temp[3] = SBOX[*last4bytes++];
+        temp[0] = SBOX[*last4bytes++];
+        temp[1] = SBOX[*last4bytes++];
+        temp[2] = SBOX[*last4bytes++];
+        temp[0] ^= RC[i];
+        lastround = roundkeys-16;
+        *roundkeys++ = temp[0] ^ *lastround++;
+        *roundkeys++ = temp[1] ^ *lastround++;
+        *roundkeys++ = temp[2] ^ *lastround++;
+        *roundkeys++ = temp[3] ^ *lastround++;
+        // k4-k7 for next round        
+        *roundkeys++ = *last4bytes++ ^ *lastround++;
+        *roundkeys++ = *last4bytes++ ^ *lastround++;
+        *roundkeys++ = *last4bytes++ ^ *lastround++;
+        *roundkeys++ = *last4bytes++ ^ *lastround++;
+        // k8-k11 for next round
+        *roundkeys++ = *last4bytes++ ^ *lastround++;
+        *roundkeys++ = *last4bytes++ ^ *lastround++;
+        *roundkeys++ = *last4bytes++ ^ *lastround++;
+        *roundkeys++ = *last4bytes++ ^ *lastround++;
+        // k12-k15 for next round
+        *roundkeys++ = *last4bytes++ ^ *lastround++;
+        *roundkeys++ = *last4bytes++ ^ *lastround++;
+        *roundkeys++ = *last4bytes++ ^ *lastround++;
+        *roundkeys++ = *last4bytes++ ^ *lastround++;
+    }
+}
+
+void aes_encrypt_128(const uint8_t *roundkeys, const uint8_t *plaintext, uint8_t *ciphertext) {
+
+    int i;
+
+    const uint32_t * rks = (uint32_t*)roundkeys;
+    const uint32_t * pt  = (uint32_t*)plaintext;
+    uint32_t t0, t1, t2, t3, u0, u1, u2, u3;
+
+    t0 = pt[0] ^ *rks++;
+    t1 = pt[1] ^ *rks++;
+    t2 = pt[2] ^ *rks++;
+    t3 = pt[3] ^ *rks++;
+
+    // 9 rounds
+    for (i = 1; i < AES_ROUNDS; ++i) {
+        u0 = T0[(uint8_t)t0] ^ T1[(uint8_t)(t1>>8)] ^ T2[(uint8_t)(t2>>16)] ^ T3[t3>>24];
+        u1 = T0[(uint8_t)t1] ^ T1[(uint8_t)(t2>>8)] ^ T2[(uint8_t)(t3>>16)] ^ T3[t0>>24];
+        u2 = T0[(uint8_t)t2] ^ T1[(uint8_t)(t3>>8)] ^ T2[(uint8_t)(t0>>16)] ^ T3[t1>>24];
+        u3 = T0[(uint8_t)t3] ^ T1[(uint8_t)(t0>>8)] ^ T2[(uint8_t)(t1>>16)] ^ T3[t2>>24];
+
+        t0 = u0 ^ *rks++;
+        t1 = u1 ^ *rks++;
+        t2 = u2 ^ *rks++;
+        t3 = u3 ^ *rks++;
+    }
+    
+    roundkeys += 160;
+    ciphertext[0]  = SBOX[(uint8_t)t0]       ^ *roundkeys++;
+    ciphertext[1]  = SBOX[(uint8_t)(t1>>8)]  ^ *roundkeys++;
+    ciphertext[2]  = SBOX[(uint8_t)(t2>>16)] ^ *roundkeys++;
+    ciphertext[3]  = SBOX[t3>>24]            ^ *roundkeys++;
+    ciphertext[4]  = SBOX[(uint8_t)t1]       ^ *roundkeys++;
+    ciphertext[5]  = SBOX[(uint8_t)(t2>>8)]  ^ *roundkeys++;
+    ciphertext[6]  = SBOX[(uint8_t)(t3>>16)] ^ *roundkeys++;
+    ciphertext[7]  = SBOX[t0>>24]            ^ *roundkeys++;
+    ciphertext[8]  = SBOX[(uint8_t)t2]       ^ *roundkeys++;
+    ciphertext[9]  = SBOX[(uint8_t)(t3>>8)]  ^ *roundkeys++;
+    ciphertext[10] = SBOX[(uint8_t)(t0>>16)] ^ *roundkeys++;
+    ciphertext[11] = SBOX[t1>>24]            ^ *roundkeys++;
+    ciphertext[12] = SBOX[(uint8_t)t3]       ^ *roundkeys++;
+    ciphertext[13] = SBOX[(uint8_t)(t0>>8)]  ^ *roundkeys++;
+    ciphertext[14] = SBOX[(uint8_t)(t1>>16)] ^ *roundkeys++;
+    ciphertext[15] = SBOX[t2>>24]            ^ *roundkeys++;
+
+}
+
+void aes_decrypt_128(const uint8_t *roundkeys, const uint8_t *ciphertext, uint8_t *plaintext) {
+
+}
